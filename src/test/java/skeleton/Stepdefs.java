eaa6af6e372f6bf9f8eedaa0a72583f0b6aa6d3d
youaxa <50586543+youaxa@users.noreply.github.com>
2019-06-25 15:02:33 +0000

    Update Stepdefs.java

--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,7 +1,6 @@
 package skeleton;
 
-import static org.junit.Assert.*;
-
+import org.junit.Assert;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
